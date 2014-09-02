0d491033aa563007c0bb32f5fb9b603f4b652b65
Martin Andrews <GitHub@mdda.net>
2014-09-03 04:21:16 +0800

    Placeholder

---
+++ procmail_with_response.js
@@ -0,0 +1,29 @@
+var MailParser = require("mailparser").MailParser;
+var mailparser = new MailParser({
+//  debug:true
+});
+var fs = require('fs');
+
+// setup an event listener when the parsing finishes
+mailparser.on("end", function(mail_object){
+  console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
+  console.log("Subject:", mail_object.subject); // Hello world!
+  console.log("Text body:", mail_object.text); // How are you today?
+  
+  if(!mail_object.attachments) {
+    console.log("NO ATTACHMENTS");
+  }
+  else {
+    for(var i=0; i<mail_object.attachments.length; i++){
+      console.log(mail_object.attachments[i].fileName, mail_object.attachments[i].length);
+    }  
+  }
+  // RESPONSE : TODO
+  
+  
+});
+
+// Read from stdin 
+process.stdin.pipe(mailparser);
+
+
